bd57db2a9ead5a215f63e93a46c09487fda18354
Diaz David sebastian <dsd228.dd@ies21.onmicrosoft.com>
2025-08-31 15:03:02 -0300

    Create data-galeria.js for image gallery
    Added a gallery of clinical and educational images with captions and credit links.

---
+++ data-galeria.js
@@ -0,0 +1,41 @@
+// data-galeria.js
+// Imágenes de galería clínica y educativa
+
+const GALERIA = [
+  {
+    caption: 'Lavado correcto de manos',
+    nivel: 'leve',
+    src: 'https://img.freepik.com/foto-gratis/primer-plano-manos-lavandose_23-2148502858.jpg',
+    credit: 'https://www.freepik.es/foto-gratis/primer-plano-manos-lavandose_23-2148502858.html'
+  },
+  {
+    caption: 'Uso correcto de guantes',
+    nivel: 'leve',
+    src: 'https://img.freepik.com/foto-gratis/cerrar-manos-guantes-latex_1150-11187.jpg',
+    credit: 'https://www.freepik.es/foto-gratis/cerrar-manos-guantes-latex_1150-11187.html'
+  },
+  {
+    caption: 'Complicaciones de no lavar manos',
+    nivel: 'fatal',
+    src: 'https://img.freepik.com/foto-gratis/doctora-mascarilla-medica-guantes-latex-lavando-manos-clinica_1157-46487.jpg',
+    credit: 'https://www.freepik.es/foto-gratis/doctora-mascarilla-medica-guantes-latex-lavando-manos-clinica_1157-46487.html'
+  },
+  {
+    caption: 'Curación de herida infectada',
+    nivel: 'fatal',
+    src: 'https://img.freepik.com/foto-gratis/infeccion-piel-curacion_1157-46486.jpg',
+    credit: 'https://www.freepik.es/foto-gratis/infeccion-piel-curacion_1157-46486.html'
+  },
+  {
+    caption: 'Vacunación infantil',
+    nivel: 'leve',
+    src: 'https://img.freepik.com/foto-gratis/enfermera-vacunando-nino_1157-46470.jpg',
+    credit: 'https://www.freepik.es/foto-gratis/enfermera-vacunando-nino_1157-46470.html'
+  },
+  {
+    caption: 'RCP básica',
+    nivel: 'leve',
+    src: 'https://img.freepik.com/foto-gratis/primeros-auxilios-reanimacion-cardiopulmonar_1157-33804.jpg',
+    credit: 'https://www.freepik.es/foto-gratis/primeros-auxilios-reanimacion-cardiopulmonar_1157-33804.html'
+  }
+];
