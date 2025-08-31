22b1794f4eed1afd91a2676b41d390dc04bfbd8b
Diaz David sebastian <dsd228.dd@ies21.onmicrosoft.com>
2025-08-31 15:03:39 -0300

    Create data-educacion.js with educational resources
    Added educational resources including infographics, videos, and PDFs related to hygiene and medical protocols.

---
+++ data-educacion.js
@@ -0,0 +1,30 @@
+// data-educacion.js
+// Recursos educativos e imágenes
+
+const EDUCACION = [
+  {
+    caption: 'Infografía: 5 momentos de higiene de manos',
+    src: 'https://img.freepik.com/vector-gratis/infografia-higiene-manos_23-2148502859.jpg',
+    credit: 'https://www.freepik.es/vector-gratis/infografia-higiene-manos_23-2148502859.html'
+  },
+  {
+    caption: 'Video: Protocolo de RCP',
+    src: 'https://img.freepik.com/vector-gratis/video-rcp_23-2148502860.jpg',
+    credit: 'https://www.freepik.es/vector-gratis/video-rcp_23-2148502860.html'
+  },
+  {
+    caption: 'PDF: Guía rápida de antibióticos',
+    src: 'https://img.freepik.com/vector-gratis/guia-antibioticos_23-2148502861.jpg',
+    credit: 'https://www.freepik.es/vector-gratis/guia-antibioticos_23-2148502861.html'
+  },
+  {
+    caption: 'Animación: Uso correcto de inhaladores',
+    src: 'https://img.freepik.com/vector-gratis/animacion-inhalador_23-2148502862.jpg',
+    credit: 'https://www.freepik.es/vector-gratis/animacion-inhalador_23-2148502862.html'
+  },
+  {
+    caption: 'Taller: Manejo de shock',
+    src: 'https://img.freepik.com/vector-gratis/taller-shock_23-2148502863.jpg',
+    credit: 'https://www.freepik.es/vector-gratis/taller-shock_23-2148502863.html'
+  }
+];
